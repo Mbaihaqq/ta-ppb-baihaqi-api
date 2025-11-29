5cd1a77c917de72cad100a50e914989b21b101dc
Jackk <Baihaqi070809@gmail.com>
2025-11-29 10:53:59 +0700

    tambah backend

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ app.use("/api/orders", orderRoutes);
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('Backend Laundry App Berjalan Normal!');
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
